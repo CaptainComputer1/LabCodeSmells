84753a7858d69ed4840030f68a8d1c3e9a9dce03
CaptainComputer1 <115894622+CaptainComputer1@users.noreply.github.com>
2023-10-29 18:21:46 -0700

    Eliminating unnecessary files
    I have eliminated the now obsolete old functions and the cognitive complexity checker (though I still thank the source!) Let's see if CodeClimate reports no issues now...

--- pytests/test_Duplicate_code_example.py
+++ pytests/test_Duplicate_code_example.py
@@ -1,15 +1,4 @@
-import time
-from Old_Code.Duplicate_code_example import *
 from New_Code.Efficient_Shape_Code import *
-def test_calculate_area():
-    assert calculate_area(3, 5) == 15
-def test_calculate_perimeter():
-    assert calculate_perimeter(3,5) == 16
-def test_calculate_volume():
-    assert calculate_volume(3,5,7) == 105
-def test_calculate_surface_area():
-    assert calculate_surface_area(3,5,7) == 142
-
 def test_calculate_shape_measurement():
     assert calculate_shape_measurement("area", 3,5) == 15
     assert calculate_shape_measurement("perimeter", 3,5) == 16
