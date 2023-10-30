affa9c3bf261e3e5c8fcf70d1f6e7efacc8b2a1e
CaptainComputer1 <115894622+CaptainComputer1@users.noreply.github.com>
2023-10-29 17:53:12 -0700

    Reduced Cognitive Complexity!
    I reduced the Cognitive Complexity of a function enough to satisfy Code Climate! Let's see if it is satisfied.

--- New_Code/Good_Smelling_Code.py
+++ New_Code/Good_Smelling_Code.py
@@ -1,7 +1,7 @@
 def calculate_total_price_new(items):
+    items = filter(lambda item: item.quantity > 0, items)
     total = 0
     for item in items:
-        if item.quantity > 0:
-            discount = 0.9 if item.price > 100 else 0.95
-            total += item.price * discount
+        discount = 0.9 if item.price > 100 else 0.95
+        total += item.price * discount
     return total
--- pytests/test_Code_smells_example.py
+++ pytests/test_Code_smells_example.py
@@ -3,6 +3,8 @@ import time
 from Old_Code.Code_smells_example import calculate_total_price
 from New_Code.Good_Smelling_Code import calculate_total_price_new
 from New_Classes.Item import Item
+from Tools.cognitive_complexity import get_cognitive_complexity
+from tabulate import tabulate
 def test_calculate_total_price():
     items = [Item("iPhone", 1, 1100.00),
              Item("Pen", 18, 7.00),
@@ -10,4 +12,13 @@ def test_calculate_total_price():
              Item("Zelda TOTK", 0, 69.99)]
     assert calculate_total_price(items) == 996.65
     assert calculate_total_price_new(items) == 996.65
+    old_total, old_details = get_cognitive_complexity(calculate_total_price)
+    print("Complexity of old price function: ")
+    print(tabulate(old_details, headers=["Complexity", "Node"], tablefmt="fancy_grid"))
+    new_total, new_details = get_cognitive_complexity(calculate_total_price_new)
+    print("Complexity of new price function: ")
+    print(tabulate(new_details, headers=["Complexity", "Node"], tablefmt="fancy_grid"))
 
+"""
+The tests for cognitive complexity were inspired by source [2].
+"""
